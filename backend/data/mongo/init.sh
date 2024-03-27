decd5625f81a5168ecd0bca70a35668735dccbb5
Sophie <sophie@chaoscookie.games>
2024-03-27 10:39:58 +0100

    mongo setup

--- backend/data/mongo/init.sh
+++ backend/data/mongo/init.sh
@@ -1,36 +1,29 @@
 
-echo "Init dabase script - Albert"
+echo "Init database script"
 
-echo "Waiting for mongo to start ..."
+echo "Waiting for MongoDB to start ..."
 while ! mongosh --quiet --eval 'db' ztbd; do
     echo "Retry in 10"
     sleep 10
 done
 
-echo "mongo online"
+echo "MongoDB online"
 
-echo "preparing data ..."
-indexes=("nvda_us" "aapl_us" "akam_us" "amd_us" "amzn_us" "ibm_us" "intc_us")
+echo "Preparing data ..."
+indexes=("Dogs" "Breeds" "Owners")
 for index in ${indexes[@]}; do
-    awk -F "\r" "NR==FNR { line=\$1; next } { print \$1 \",\" line }" \
-     /root/data/xgarbage.bytes /root/data/$index"_d.csv" > /root/data/mg_$index"_d.csv"
+    cp /root/data/${index}.csv /root/data/mg_${index}.csv
 done
 
-awk -F "\r" "{ print \$1 \",cafe\" }" \
- /root/data/spx_d.csv > /root/data/mg_spx_d.csv
-
-echo "loading data ..."
+echo "Loading data ..."
 
 mongosh --quiet -u root -p example --authenticationDatabase admin -f /root/data/load.js ztbd
 
 for index in ${indexes[@]}; do
-    mongoimport --collection=$index"_data" --db='ztbd' \
-     --columnsHaveTypes --fieldFile=/root/data/fields --file=/root/data/mg_$index"_d.csv" --type=csv \
+    mongoimport --collection=${index} --db='ztbd' \
+     --columnsHaveTypes --fieldFile=/root/data/fields --file=/root/data/mg_${index}.csv --type=csv \
      -u root -p example --authenticationDatabase=admin
 done
 
-mongoimport --collection=spx_data --db='ztbd' \
- --columnsHaveTypes --fieldFile=/root/data/fields --file=/root/data/mg_spx_d.csv --type=csv \
- -u root -p example --authenticationDatabase=admin
-
 echo "Probably loaded :P"
+
